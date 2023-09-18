2a8556d402c1258858b284c41b529c9e3d574299
shmsacr <shmsacr@gmail.com>
2023-09-18 15:04:10 +0300

    feat: create home page

---
+++ lib/screens/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:restapi_crud/screens/add_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Todo list"),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: navigateToAddPage, label: Text("Add todo")),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
