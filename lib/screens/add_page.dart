b9bd2889b031a81a5638ed14ae3627833028f952
shmsacr <shmsacr@gmail.com>
2023-09-28 18:19:34 +0300

    feat: add put method

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
-  const AddTodoPage({Key? key}) : super(key: key);
+  final Map? todo;
+  const AddTodoPage({Key? key, this.todo}) : super(key: key);
 
   @override
   State<AddTodoPage> createState() => _AddTodoPageState();
@@ -13,11 +14,26 @@ class AddTodoPage extends StatefulWidget {
 class _AddTodoPageState extends State<AddTodoPage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
+  bool isEdit = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    final todo = widget.todo;
+    if (todo != null) {
+      isEdit = true;
+      final title = todo["title"];
+      final description = todo["description"];
+      titleController.text = title;
+      descriptionController.text = description;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Add Todo"),
+        title: Text(isEdit ? "Edit todo" : "Add Todo"),
       ),
       body: ListView(
         padding: EdgeInsets.all(20),
@@ -36,13 +52,46 @@ class _AddTodoPageState extends State<AddTodoPage> {
           SizedBox(
             height: 16,
           ),
-          ElevatedButton(onPressed: submitData, child: Text("Submit"))
+          ElevatedButton(
+              onPressed: isEdit ? updateData : submitData,
+              child: Text(isEdit ? "Update" : "Submit"))
         ],
       ),
     );
   }
 
-  void submitData() async {
+  Future<void> updateData() async {
+    final todo = widget.todo;
+    if (todo == null) {
+      return;
+    }
+    final id = todo["_id"];
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final url = "https://api.nstack.in/v1/todos/$id";
+    final body = {
+      "title": title,
+      "description": description,
+      "isComplete": false
+    };
+    final uri = Uri.parse(url);
+    final response = await http.put(
+      uri,
+      body: jsonEncode(body),
+      headers: {"Content-Type": 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      print(response.statusCode);
+      titleController.text = "";
+      descriptionController.text = "";
+      showSuccessMessage("updation Succes");
+    } else {
+      print(response.body);
+      showErrorMessage("updation Fail");
+    }
+  }
+
+  Future<void> submitData() async {
     final title = titleController.text;
     final description = descriptionController.text;
     final url = "https://api.nstack.in/v1/todos";
